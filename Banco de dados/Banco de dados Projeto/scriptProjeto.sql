2554ece6c549a2e29a45da99e1c7acc3f6af6d35
GabrielNicodemos <gabriel.nicodemos@bandtec.com.br>
2020-10-12 18:40:42 -0300

    Mudanças no banco de dados

--- Banco de dados/Banco de dados Projeto/scriptProjeto.sql
+++ Banco de dados/Banco de dados Projeto/scriptProjeto.sql
@@ -3,31 +3,31 @@ use projeto;
 
 
 -- Criação das tabelas do projeto
-create table cadastro(
-idCadastro int primary key auto_increment,
+create table usuario(
+idUser int primary key auto_increment,
 nome varchar(50) not null,
 dtNasc date,
-telefone char(13)
+telefoneCel char(13),
+telefoneFixo char(13),
+email varchar(50) not null,
+senha varchar(20) not null
 );
 
-create table login(
-idLogin int primary key auto_increment
-);
 
-create table loginCadastro(
-  fkLogin int,
-  foreign key (fkLogin) references login(idLogin),
-  fkCadastro int,
-  foreign key (fkCadastro) references cadastro(idCadastro),
-  primary key (fkLogin, fkCadastro),
-  email varchar(50) not null,
-  senha varchar(20) not null
-);
+
+
 
 create table sensor (
 idSensor int primary key auto_increment,
 container int
 );
+
+
+-- Adicionando uma coluno fk na tabela sensor
+alter table sensor add column fkUsuario int;
+
+-- Referenciando fkUsuario no idUser
+alter table sensor add foreign key (fkUsuario) references usuario(idUser);
 
 
 create table medicoes (
@@ -40,23 +40,12 @@ foreign key (fkSensor) references sensor(idSensor)
 
 -- Inserindo dados na tabela
 
-insert into cadastro values
-(null,'leonidas','2001-02-01','(11)945762345'),
-(null,'natanael','1999-12-20','(11)945675341'),
-(null,'bozo','1930-05-17','(11)978543217'),
-(null,'elton','1955-10-18','(11)978654326');
+insert into usuario values
+(null,'leonidas','2001-02-01','(11)945762345', '(11)44445555', 'leonidas@gmail.com', 'senha1234'),
+(null,'natanael','1999-12-20','(11)945675341', '(11)55556666', 'natanael@hotmail.com', 'password2020'),
+(null,'bruno','1930-05-17','(11)978543217', '(11)88889999', 'bruno@gmail.com', 'bruno2020'),
+(null,'elton','1955-10-18','(11)978654326', '(11)11112222', 'elton@bandtec.com.br', '123456789');
 
-insert into login values
-(null),
-(null),
-(null),
-(null);
-
-insert into loginCadastro values
-(1, 1, 'felipe@gmail.com', 'felipe123'),
-(2, 2, 'matheus@bandtec.com.br' , 'senha20'),
-(3, 3, 'gabriel@hotmail.com', 'password18'),
-(4, 4, 'lucas@yahoo.com', 'senha2020' );
 
 insert into sensor values
 (null, 6),
@@ -64,6 +53,13 @@ insert into sensor values
 (null, 31),
 (null, 54),
 (null, 22);
+
+update sensor set fkUsuario = 1 where idSensor = 1;
+update sensor set fkUsuario = 2 where idSensor in (2,4);
+update sensor set fkUsuario = 3 where idSensor = 5;
+update sensor set fkUsuario = 4 where idSensor = 3;
+
+
 
 insert into medicoes values
 (null, 24, '2020-07-17 10:50:30', 2),
@@ -74,31 +70,25 @@ insert into medicoes values
 
 
 -- select das tabelas
-select * from login;
-select * from cadastro;
+
+
 select * from sensor;
 select * from medicoes;
+select * from usuario;
 select * from sensor, medicoes where idsensor = fkSensor;
-select * from loginCadastro;
 
 select sensor.idSensor , medicoes.temperatura 
 from sensor, medicoes
 where idsensor = fkSensor;
 
-select * from loginCadastro;
+select usuario.* , sensor.idSensor
+from usuario, sensor
+where  idUser = fkUsuario and idSensor in (2,4);
 
-select login.idLogin, cadastro.nome, loginCadastro.email, loginCadastro.senha
-from login, cadastro, loginCadastro
-where fkLogin = idLogin and fkCadastro = idCadastro;
+select usuario.* , sensor.idSensor
+from usuario, sensor
+where  idUser = fkUsuario and idSensor = 3;
 
-
-
-
-
-
-
-
-
-
-
-
+select usario.* , sensor.idSensor 
+from usuario, sensor
+where  idUser = fkUsuario;
